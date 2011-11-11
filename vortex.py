1b0bd507e26e89ffb35cf48be8d3c96b571e1897
Noah Zoschke <noah@heroku.com>
2011-11-10 19:43:53 -0800

    unescape urls; debug querying

--- vortex.py
+++ vortex.py
@@ -1,12 +1,12 @@
 import os
 import requests
+from urllib import unquote
 from urlparse import urlparse
 
 from BeautifulSoup import BeautifulSoup
 from beets import Library
 
 APP_DIR = os.path.abspath(os.path.join(__file__, ".."))
-LIB = Library(path="%s/musiclibrary.blb" % APP_DIR, directory="%s/davroot" % APP_DIR)
 
 class SpotifyLink(object):
   title       = None
@@ -19,9 +19,13 @@ class SpotifyLink(object):
   def __init__(self, url):
     self.url = urlparse(url)
     if self.url.path.startswith("/local"):
-      path = self.url.path.replace("+", " ")
+      path = unquote(self.url.path)
+      path = path.replace("+", " ")
+      path = path.replace("(", " ")
+      path = path.replace(")", " ")
       _, _, self.artist, self.album, self.title, _ = path.split("/")
     else:
+      print "SONG URL: %s" % self.url.geturl()
       song = BeautifulSoup(requests.get(self.url.geturl()).content)
       self.title    = song.find("meta", property="og:title")["content"]
       self.track    = int(song.find("meta", property="music:album:track")["content"])
@@ -38,8 +42,11 @@ class SpotifyLink(object):
       self.artist = artist.find("meta", property="og:title")["content"]
 
   def path(self):
-    query = "title:%s artist:%s album:%s" % (self.title, self.artist, self.album)
-    paths = [i.path for i in LIB.items(query=query)]
+    lib = Library(path="%s/musiclibrary.blb" % APP_DIR, directory="%s/davroot" % APP_DIR)
+    query = "%s %s %s" % (self.title, self.artist, self.album)
+    paths = [i.path for i in lib.items(query=query)] + [None]
+    print "SONG QUERY: %s" % query
+    print "SONG PATH:  %s" % paths[0]
     return paths[0]
 
   def __str__(self):

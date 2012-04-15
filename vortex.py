423e912fc1dbcd1e0ad81f04acbe2d53f286b6e2
Noah Zoschke <noah@heroku.com>
2012-04-15 13:13:45 -0700

    split before url parsing

--- vortex.py
+++ vortex.py
@@ -15,32 +15,36 @@ class SpotifyLink(object):
   albumartist = None 
   track       = 0
   duration    = 0
-
+  
   def __init__(self, url):
     self.url = urlparse(url)
     if self.url.path.startswith("/local"):
-      path = unquote(self.url.path)
-      path = path.replace("+", " ")
-      path = path.replace("(", " ")
-      path = path.replace(")", " ")
-      _, _, self.artist, self.album, self.title, _ = path.split("/")
+      _, _, self.artist, self.album, self.title, _ = self.url.path.split("/")
+      for attr in ("artist", "album", "title"):
+        s = self.__getattribute__(attr)
+        s = unquote(s)
+        s = s.replace("+", " ")
+        s = s.replace("(", " ")
+        s = s.replace(")", " ")
+        self.__setattr__(attr, s)
+      print self
     else:
       print "SONG URL: %s" % self.url.geturl()
       song = BeautifulSoup(requests.get(self.url.geturl()).content)
       self.title    = song.find("meta", property="og:title")["content"]
       self.track    = int(song.find("meta", property="music:album:track")["content"])
       self.duration = int(song.find("meta", property="music:duration")["content"])
-
+      
       # query for album
       album_url = song.find("meta", property="music:album")["content"]
       album = BeautifulSoup(requests.get(album_url).content)
       self.album = album.find("meta", property="og:title")["content"]
-
+      
       # query for album artist
       artist_url = song.find("meta", property="music:musician")["content"]
       artist = BeautifulSoup(requests.get(artist_url).content)
       self.artist = artist.find("meta", property="og:title")["content"]
-
+  
   def path(self):
     lib = Library(path="%s/musiclibrary.blb" % APP_DIR, directory="%s/davroot" % APP_DIR)
     query = "%s %s %s" % (self.title, self.artist, self.album)
@@ -48,6 +52,6 @@ class SpotifyLink(object):
     print "SONG QUERY: %s" % query
     print "SONG PATH:  %s" % paths[0]
     return paths[0]
-
+  
   def __str__(self):
     return u"%s/%s/%s" % (self.artist, self.album, self.title)
